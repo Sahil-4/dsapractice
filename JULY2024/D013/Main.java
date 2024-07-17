520dcbe03f2b728b3db17df4c422276cb4b7b75e
Sahil-4 <www.sahil.875020@gmail.com>
2024-07-17 12:45:50 +0530

    fix : Day 13 added missing imports

--- JULY2024/D013/Main.java
+++ JULY2024/D013/Main.java
@@ -1,5 +1,9 @@
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map;
+import java.util.Stack;
+
 class StockSpanner {
-    Stack<Pair<Integer, Integer>> prices = new Stack<>();
+    Stack<Map.Entry<Integer, Integer>> prices = new Stack<>();
 
     public StockSpanner() {}
     
@@ -11,7 +15,7 @@ class StockSpanner {
         }
 
         if (prices.isEmpty() || prices.peek().getKey() > price) {
-            Pair p = new Pair(price, ++span);
+            Map.Entry<Integer, Integer> p = new SimpleEntry<>(price, ++span);
             prices.push(p);
         }
 
@@ -20,13 +24,13 @@ class StockSpanner {
 }
 
 public class Main {
-    public void p1() {
+    public static void p1() {
         // Problem 1 : Leetcode 901. Online Stock Span - https://leetcode.com/problems/online-stock-span/ 
         // Geeksforgeeks - https://www.geeksforgeeks.org/problems/stock-span-problem-1587115621/1 
 
         int arr[] = { 10, 4, 5, 90, 120, 80 };
-        int spans[];
-        StockSpanner ss = StockSpanner();
+        int spans[] = {};
+        StockSpanner ss = new StockSpanner();
         for (int i = 0; i < arr.length; i++) spans[i] = ss.next(arr[i]);
         for (int i = 0; i < arr.length; i++) System.out.println(spans[i]);
     }
